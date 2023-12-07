24198b8904ebd0319e750dbe7ede90a7029aac5d
Vansh Shah <vanshshah842@gmail.com>
2023-12-07 13:46:44 +0530

    first commit

---
+++ authentication/auth_system/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomePage, Register, Login, logoutuser
+
+urlpatterns = [
+    path('home/', HomePage, name="home-page"),
+    path('register/', Register, name="register-page"),
+    path('login/', Login, name="login-page"),
+    path('logout/', logoutuser, name='logout')
+]
